15413696e437bc6421a2fae752721d318fc4f08f
samlafontaine <sam@samlafontaine.com>
2024-02-18 12:09:46 +0100

    change text color

--- app/page.tsx
+++ app/page.tsx
@@ -195,7 +195,7 @@ export default function Home() {
           <p className="text-2xl font-medium bg-gradient-to-r from-cyan-500 to-blue-500 inline-block text-transparent bg-clip-text mb-1">
             Fifa tournament
           </p>
-          <p className="text-sm text-muted-foreground leading-relaxed">
+          <p className="text-sm text-zinc-600 leading-relaxed">
             Information about the ongoing Fifa tournament. Add and view recently
             played matches, and see who's currently leading in the rankings.
           </p>
